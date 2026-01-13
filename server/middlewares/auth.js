64b1b4c1c465dce6c89ff7ac9c2bc2c5f9065c70
RachitJ05 <144156188+RachitJ05@users.noreply.github.com>
2026-01-14 01:49:26 +0530

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,36 +1,16 @@
 import { clerkClient } from "@clerk/express";
 
 export const auth = async (req, res, next) => {
-  
   try {
-    const authData = await req.auth();
-    const userId = authData.userId;
-
-    let hasPremiumPlan = false;
-
-    try {
-      if (authData.has) {
-        hasPremiumPlan = await authData.has({ plan: "premium" });
-      }
-    } catch {
-      hasPremiumPlan = false;
-    }
-
-    const user = await clerkClient.users.getUser(userId);
-
-    let free_usage = user.privateMetadata?.free_usage ?? 0;
-
-    if (!hasPremiumPlan && free_usage >= 10) {
-      return res.status(403).json({
+    const userId = await req.auth().userId;
+    if (!userId) {
+      return res.status(401).json({
         success: false,
-        message: "Limit reached. Upgrade to continue.",
+        message: "Unauthorized",
       });
     }
-
-    req.plan = hasPremiumPlan ? "premium" : "free";
-    req.free_usage = free_usage;
+    
     req.userId = userId;
-
     next();
   } catch (error) {
     console.error("AUTH ERROR:", error);
